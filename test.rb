ac99b0f3906199b4ea9ad8676dfb231ad6dfa506
mygithubangebenie <angebenie23@gmail.com>
2019-08-01 14:23:41 +0200

    webtechnology tasks third commit correction

--- test.rb
+++ test.rb
@@ -15,7 +15,7 @@ server = WEBrick::HTTPServer.new({
           Signal.trap(signal){ server.shutdown }
         }
 
-server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
 server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
 server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
 server.mount('/goya1.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya1.rb')
